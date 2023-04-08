b1131da203e12d70d4392ab7891ea08b2b5d38dc
negi-tushar <negitushar44@gmail.com>
2023-04-09 00:46:43 +0530

    added code

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sports/calen.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -27,23 +28,50 @@ class _HomePageState extends State<HomePage> {
         ),
         body: Column(
           children: [
-            
-            // Container(
-            //   child: const Text(
-            //     "LIVE Match",
-            //     style: TextStyle(color: Colors.white,),
-            //     textAlign: TextAlign.left,
-            //   ),
-            // ),
-            // Padding(
-            //   padding: EdgeInsets.all(8),
-            //   child: Container(
-            //     height: 200,
-            //     decoration: const BoxDecoration(
-            //         color: Colors.purple,
-            //         borderRadius: BorderRadius.all(Radius.circular(20))),
-            //   ),
-            // ),
+            DatePickerCustom(),
+            Container(
+              margin: const EdgeInsets.only(left: 20),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                "LIVE Match",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return LiveMatchCard();
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ListTile(
+              iconColor: Colors.white,
+              textColor: Colors.white,
+              leading: Icon(Icons.cabin),
+              title: Text('Today match'),
+              trailing: Icon(Icons.navigate_next_outlined),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return TodayMatchWidget();
+                },
+              ),
+            ),
           ],
         ),
 
@@ -88,6 +116,89 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class TodayMatchWidget extends StatelessWidget {
+  const TodayMatchWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(15),
+        margin: const EdgeInsets.all(10),
+        height: 80,
+        width: MediaQuery.of(context).size.width * .9,
+        decoration: const BoxDecoration(
+            color: Colors.white12,
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('N Forest'),
+            Icon(Icons.trending_neutral),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.green.shade600, width: 2),
+                  color: Colors.green.shade400,
+                  borderRadius: BorderRadius.circular(20)),
+              child: const Text('06:30'),
+            ),
+            Icon(Icons.nat_outlined),
+            Text('Liverpool'),
+          ],
+        ));
+  }
+}
+
+class LiveMatchCard extends StatelessWidget {
+  const LiveMatchCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      margin: const EdgeInsets.all(10),
+      height: 200,
+      width: MediaQuery.of(context).size.width * .9,
+      decoration: const BoxDecoration(
+          color: Colors.purple,
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      child: Column(
+        children: [
+          Text('Stamford Bridge'),
+          Text('week 10'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [Icon(Icons.tab), Text('Chelsea'), Text('Home')],
+              ),
+              Column(
+                children: [
+                  Text('1 : 1'),
+                  Container(
+                    decoration: BoxDecoration(
+                        border:
+                            Border.all(color: Colors.green.shade600, width: 2),
+                        color: Colors.green.shade400,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Text('90+4'),
+                  )
+                ],
+              ),
+              Column(
+                children: [Icon(Icons.tab), Text('Man Utd'), Text('Away')],
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
