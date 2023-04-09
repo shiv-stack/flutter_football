f7057d30aad8361c970acba7370b953ee78a3034
negi-tushar <negitushar44@gmail.com>
2023-04-09 13:23:05 +0530

    commit all

--- lib/https/services.dart
+++ lib/https/services.dart
@@ -3,7 +3,8 @@ import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:flutter_sports/models/live_score_model.dart';
 
-const String apiKey = '';
+const String apiKey =
+    'b393d35a95cd406d8e162c139a6c39b46993ac37e042015c8335699dd8996ad6';
 
 class Services {
   final _dio = Dio();
@@ -11,15 +12,11 @@ class Services {
   Future<LiveScoreDetails?> getLiveScore() async {
     LiveScoreDetails? details;
     // try {
-    log('called======>');
     final response = await _dio.get(
         'https://apiv2.allsportsapi.com/football/?met=Livescore&APIkey=$apiKey');
 
-    log('called======> ' + response.statusCode.toString());
-
     if (response.statusCode == 200) {
       details = LiveScoreDetails.fromJson(response.data);
-      log('success' + details.result.length.toString());
     } else {
       log('failed!!!!!');
     }
